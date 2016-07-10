88799b06afc677b5c3efa99e0b5dc25e94d4f23c
VikingProgrammer <benobe22@gmail.com>
2016-07-10 22:32:24 +0100

    Added new Data

--- Refugee_Map_09_07_2016/code/World_Map.js
+++ Refugee_Map_09_07_2016/code/World_Map.js
@@ -6,7 +6,7 @@ var path;
 var svg
 var tooltip
 var width = screen.width, height = screen.height;
-var filePath = "https://raw.githubusercontent.com/VikingProgrammer/Data_Data_Everywhere/master/Refugee_Map_09_07_2016/data/countries.geojson"
+var filePath = "https://raw.githubusercontent.com/VikingProgrammer/Data_Data_Everywhere/master/Refugee_Map_09_07_2016/data/countries.json"
 
 
 projection = d3.geo.albers()
@@ -16,24 +16,21 @@ projection = d3.geo.albers()
 .scale(600)
 .translate([width / 2, height / 2]);
 
+projection = d3.geo.mercator()
+.center([0, 5 ])
+.scale(900)
+.rotate([-180,0]);
+
 path = d3.geo.path().projection(projection);
 
-svg = d3.select("#map_area").append("svg")
-.attr("width", width)
-.attr("height", height);
-tooltip = d3.select('body').append('div')
-.attr('class', 'hidden tooltip');
+svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
 
-
-d3.json(filePath, function(error, countries) {
-    console.log(filePath);
+d3.json("https://raw.githubusercontent.com/VikingProgrammer/Data_Data_Everywhere/master/Refugee_Map_09_07_2016/data/countries.json", function(error, countries) {
     console.log(countries)
     if (error) return console.error(error);
 
-    alert(countries.features)
-    svg.selectAll("path")
-    .data(countries.features)
-    .enter().append("path")
-    .attr("class", function(d) { return "path " + d.id; })
-    .attr("d", path)
+    var subunits = topojson.feature(countries, countries.objects.countries)
+    console.log(subunits)
+    svg.selectAll("path").data(subunits).enter().append("path").attr("d", path)
+    //.attr("class", function(d) { return "path " + d.id; })
 });
