36652f9271fb0576aa65f4532ee852d0bed86628
VikingProgrammer <benobe22@gmail.com>
2016-07-12 23:00:12 +0100

    Testing removal of antarctica

--- Refugee_Map_09_07_2016/code/World_Map.js
+++ Refugee_Map_09_07_2016/code/World_Map.js
@@ -6,31 +6,22 @@ var path;
 var svg
 var tooltip
 var width = screen.width, height = screen.height;
-var filePath = "https://raw.githubusercontent.com/VikingProgrammer/Data_Data_Everywhere/master/Refugee_Map_09_07_2016/data/countries.json"
-
-
-projection = d3.geo.albers()
-.center([1.0,53])
-.rotate([4.4, 0])
-.parallels([50, 60])
-.scale(600)
-.translate([width / 2, height / 2]);
 
 projection = d3.geo.mercator()
-.center([0, 5 ])
-.scale(900)
-.rotate([-180,0]);
-
+.center([3.0,10])
+.rotate([-370,-6])
+.scale(100)
+.translate([width / 2.4, height / 2])
 path = d3.geo.path().projection(projection);
 
-svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+svg = d3.select("#map_area").append("svg").attr("width", width).attr("height", height);
 
-d3.json("https://raw.githubusercontent.com/VikingProgrammer/Data_Data_Everywhere/master/Refugee_Map_09_07_2016/data/countries.json", function(error, countries) {
+d3.json("https://raw.githubusercontent.com/VikingProgrammer/Data_Data_Everywhere/master/Refugee_Map_09_07_2016/data/custom.json", function(error, countries) {
     console.log(countries)
     if (error) return console.error(error);
 
-    var subunits = topojson.feature(countries, countries.objects.countries)
-    console.log(subunits)
-    svg.selectAll("path").data(subunits).enter().append("path").attr("d", path)
+    //var subunits = topojson.feature(countries, countries.objects.countries)
+    console.log(countries.features)
+    svg.selectAll("path").data(countries.features).enter().append("path").attr("d", path)
     //.attr("class", function(d) { return "path " + d.id; })
 });
